8aa3006a36763153a20539bb9f8bdf0e4a745b95
Berzan <berzan@tutanota.com>
2023-10-04 15:30:13 +0000

    create: `lib.rs` file

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod execute;
+pub mod query;
+pub mod state;
+
+use cosmwasm_std::{
+    entry_point, to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+};
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty,
+) -> StdResult<Response> {
+    Ok(Response::new())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: query::QueryMsg) -> StdResult<Binary> {
+    match msg {
+        query::QueryMsg::AddrByName { name } => to_binary(&query::addr_by_name(deps, name)?),
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: execute::ExecuteMsg,
+) -> StdResult<Response> {
+    match msg {
+        execute::ExecuteMsg::RegisterName { name } => execute::register_name(deps, info, name),
+        execute::ExecuteMsg::UnregisterName { name } => execute::unregister_name(deps, info, name),
     }
 }
